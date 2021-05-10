60a05d7aefa2cead9345813b91e703c38970603b
vladimir9255 <70789419+darwin12112@users.noreply.github.com>
2021-05-10 10:29:54 +0900

    fixed

--- src/variables/publicFetch.js
+++ src/variables/publicFetch.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from "axios"
 
-const baseURL = process.env.REACT_APP_URL;
-const staticURL = process.env.REACT_APP_URL;
+const baseURL = process.env.REACT_APP_BASE_URL;
+const staticURL = process.env.REACT_APP_STATIC_URL;
 
 const publicFetch = axios.create({
-  baseURL,
+  baseURL
 });
 
 export { publicFetch, baseURL, staticURL };
